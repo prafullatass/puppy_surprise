f68b913c0449292e497f557e6d9647c3c7f35732
Madi Peper <madisonpeper@gmail.com>
2019-01-31 09:38:53 -0600

    added instructions

--- src/main.js
+++ src/main.js
@@ -1,14 +1,1 @@
-// Create a function that will toggle the classes for the puppy!
-
-let puppy = document.querySelector("#pup-img");
-document.querySelector("#toggle-btn").addEventListener("click", () => {
-    if(puppy.className === "hidden"){
-        // make puppy visible
-        puppy.setAttribute("class", "visible");
-    } else {
-        // make puppy disappear
-        document.querySelector("#pup-img").setAttribute("class", "hidden");
-    }
-    // }
-
-})
+// Create a function that will toggle the puppy from hidden to visible
